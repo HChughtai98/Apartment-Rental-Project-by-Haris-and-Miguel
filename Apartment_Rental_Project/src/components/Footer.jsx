0e68748432ee258aeba56ec7fad7499e9b5456ed
Haris Chughtai <xharisahmed98@gmail.com>
2023-12-12 18:31:31 +0000

    added header sidebar footer

--- Apartment_Rental_Project/src/components/Footer.jsx
+++ Apartment_Rental_Project/src/components/Footer.jsx
@@ -1,7 +1,14 @@
-
+import React from "react";
+import "/src/styles/Footer.css";
 
 const Footer = () => {
-  return "test";
+  return (
+    <div className="FooterContainer">
+      <div className="FooterDiv">
+        <h4>IT-Service</h4>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- Apartment_Rental_Project/src/components/Sidebar.jsx
+++ Apartment_Rental_Project/src/components/Sidebar.jsx
@@ -2,7 +2,15 @@ import React from "react";
 import "/src/styles/Sidebar.css";
 
 const Sidebar = () => {
-  return "sidebar";
+  return (
+    <div className="SidebarContainer">
+      <div className="SidebarDiv">
+        <p>Home</p>
+        <p>Appartments</p>
+        <p>Favourites</p>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
