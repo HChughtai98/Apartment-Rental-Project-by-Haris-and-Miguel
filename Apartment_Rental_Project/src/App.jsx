2c98ab64f3bf81c6aef60904fee186a43fe9f966
Haris Chughtai <xharisahmed98@gmail.com>
2023-12-12 21:17:55 +0000

    pushing

--- Apartment_Rental_Project/src/App.jsx
+++ Apartment_Rental_Project/src/App.jsx
@@ -9,6 +9,7 @@ function App() {
     <>
       <div>
         <Header />
+        {/*        <Screen /> */}
         <Sidebar />
         <Footer />
       </div>
---
+++ Apartment_Rental_Project/src/components/Screen.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "/src/styles/Screen.css";
+
+const Screen = () => {
+  return <div className="ScreenContainer"></div>;
+};
+
+export default Screen;
